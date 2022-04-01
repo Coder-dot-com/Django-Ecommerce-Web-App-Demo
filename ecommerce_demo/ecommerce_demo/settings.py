18423e441f20e16d6f3e5ee3db642f2bdbc5013d
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-04-01 15:57:50 +0100

    began rendering home page

--- ecommerce_demo/ecommerce_demo/settings.py
+++ ecommerce_demo/ecommerce_demo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ecommerce_demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,6 +79,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
@@ -116,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+
+STATICFILES_DIRS = [
+    'ecommerce_demo/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
